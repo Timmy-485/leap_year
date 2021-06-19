e102eff71b60844632cf98992d6e4a3c473510c4
Timmy-485 <timothy.yeboah@amalitech.org>
2021-06-19 18:35:42 +0000

    redid UI

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
 let leapYear = () => {
     let year = document.getElementById("input").value;
     let output = document.getElementById("output");
+    const imgField = document.getElementById("outputImage")
     output.classList.add("showOutput")
+    imgField.classList.add("showOutput")
 
     let state = false;
     if((year>=1)){
@@ -10,17 +12,19 @@ let leapYear = () => {
                 if(year%400 == 0){
                     state = true;
                 }
+            } else{
+                state = true;
             }
         }
     }
-    else {
-        state = false;
-    }
+
 
     if(state){
         output.value = `${year} is a leap year`
+        imgField.src = "../images/isLeap.png"
     }
     else{
         output.value = `${year} is not a leap year`
+        imgField.src = "../images/isNotLeap.jpg"
     }
 }
